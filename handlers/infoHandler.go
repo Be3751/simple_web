d82fd6dfbe9e57018d796780ba78c331e4d0a725
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-02-07 03:49:59 +0900

    put a message in Info page and added some debug messages

--- handlers/infoHandler.go
+++ handlers/infoHandler.go
@@ -9,15 +9,19 @@ import (
 )
 
 func InfoHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Access to /info")
+
 	fmt.Println("start to read a file")
 	f, err := os.Open("templates/info.html")
 	if err != nil {
 		fmt.Println("error")
 	}
-	defer f.Close()
+	defer func() {
+		fmt.Println("finish reading a file")
+		f.Close()
+	}()
 
 	b, err := ioutil.ReadAll(f)
 	s := string(b)
 	io.WriteString(w, s)
-
 }
